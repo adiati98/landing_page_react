da88f0ed56fac7ca5938b0d22d3fd2ed418415a0
Retno Ayu Adiati <retno_ayu@hotmail.com>
2022-07-06 02:00:50 +0200

    Add semantic HTML to CardsContainer component * Change CSS classNames in CardsContainer * Fix styling in cards-container to use grid and set media queries

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -3,157 +3,167 @@ import circle from '../assets/svg/circle.svg'
 
 export default function CardsContainer({ cardsData }) {
 	return (
-		<div className='cards__container'>
-			<div className='card__container'>
+		<section className='cards__container'>
+			<div className='card__grid'>
 				{cardsData.cardImages1.map((card) => (
-					<div className='card__container__image' key={card.id}>
+					<article className='card__article' key={card.id}>
 						<img className='card__image' src={card.image} alt={card.altText} />
-						<div className='card__image__text__container'>
-							<p className='card__image__text card__image__text__name'>
-								{card.companyName}
-							</p>
-							<p className='card__image__text card__image__text__title'>
-								{card.title}
-							</p>
+						<div className='card__text__container'>
+							<h1>
+								<span className='small card__text__name'>{card.companyName}</span>
+								<span className='card__text__title'>{card.title}</span>
+							</h1>
 							<a href='#blog' className='show-link'>
 								<img src={circle} alt='' className='circle' />
 								<span className='read-more'>Read more</span>
 							</a>
 						</div>
-					</div>
+					</article>
 				))}
 			</div>
-			<div className='card__container__break card__container__break--reverse'>
-				<div className='card__container__text__break'>
+			{/* flex card */}
+			<div className='card__flex--reverse'>
+				<section className='card__flex__list p-2'>
 					{cardsData.cardText.map((card) => (
-						<div className='card__text__flex' key={card.id}>
-							<p className='card__text__flex__name'>{card.companyName}</p>
-							<p className='card__text__flex__description'>{card.text}</p>
+						<article className='card__flex__article' key={card.id}>
+							<h1>
+								<span className='card__flex__name'>{card.companyName}</span>
+								<span className='card__flex__title'>{card.text}</span>
+							</h1>
 							<a href='#blog' className='show-link'>
 								<img src={circle} alt='' className='circle' />
 								<span className='read-more'>Read more</span>
 							</a>
-						</div>
+						</article>
 					))}
-				</div>
-				<div
-					className='card__container__image__break'
+				</section>
+				<article
+					className='card__article card__flex__article'
 					key={cardsData.florensis[0].id}
 				>
 					<img
-						className='card__image__flex'
+						className='card__image card__flex__image'
 						src={cardsData.florensis[0].image}
 						alt={cardsData.florensis[0].altText}
 					/>
-					<div className='image__text__flex'>
-						<p className='image__text__flex__name'>
-							{cardsData.florensis[0].companyName}
-						</p>
-						<p className='image__text__flex__title'>{cardsData.florensis[0].title}</p>
+					<div className='card__text__container card__flex__text__container'>
+						<h1>
+							<span className='small card__text__name'>
+								{cardsData.florensis[0].companyName}
+							</span>
+							<span className='card__text__title'>
+								{cardsData.florensis[0].title}
+							</span>
+						</h1>
 						<a href='#blog' className='show-link'>
 							<img src={circle} alt='' className='circle' />
 							<span className='read-more'>Read more</span>
 						</a>
 					</div>
-				</div>
+				</article>
 			</div>
-			<div className='card__container'>
+			<div className='card__grid'>
 				{cardsData.cardImages2.map((card) => (
-					<div className='card__container__image' key={card.id}>
-						<img className='card__image__flex' src={card.image} alt={card.altText} />
-						<div className='card__image__text__container'>
-							<p className='card__image__text card__image__text__name'>
-								{card.companyName}
-							</p>
-							<p className='card__image__text card__image__text__title'>
-								{card.title}
-							</p>
+					<article className='card__article' key={card.id}>
+						<img className='card__image' src={card.image} alt={card.altText} />
+						<div className='card__text__container'>
+							<h1>
+								<span className='small card__text__name'>{card.companyName}</span>
+								<span className='card__text__title'>{card.title}</span>
+							</h1>
 							<a href='#blog' className='show-link'>
 								<img src={circle} alt='' className='circle' />
 								<span className='read-more'>Read more</span>
 							</a>
 						</div>
-					</div>
+					</article>
 				))}
 			</div>
-			<div className='card__container__break'>
-				<div className='card__container__text__break'>
+			{/* flex card */}
+			<div className='card__flex reverse'>
+				<section className='card__flex__list p-2'>
 					{cardsData.cardText.map((card) => (
-						<div className='card__text__flex' key={card.id}>
-							<p className='card__text__flex__name'>{card.companyName}</p>
-							<p className='card__text__flex__description'>{card.text}</p>
+						<article className='card__flex__article' key={card.id}>
+							<h1>
+								<span className='card__flex__name'>{card.companyName}</span>
+								<span className='card__flex__title'>{card.text}</span>
+							</h1>
 							<a href='#blog' className='show-link'>
 								<img src={circle} alt='' className='circle' />
 								<span className='read-more'>Read more</span>
 							</a>
-						</div>
+						</article>
 					))}
-				</div>
-				<div
-					className='card__container__image__break'
+				</section>
+				<article
+					className='card__article card__flex__article'
 					key={cardsData.florensis[1].id}
 				>
 					<img
-						className='card__image__flex'
+						className='card__image card__flex__image'
 						src={cardsData.florensis[1].image}
 						alt={cardsData.florensis[1].altText}
 					/>
-					<div className='image__text__flex'>
-						<p className='image__text__flex__name'>
-							{cardsData.florensis[1].companyName}
-						</p>
-						<p className='image__text__flex__title'>{cardsData.florensis[0].title}</p>
+					<div className='card__text__container card__flex__text__container'>
+						<h1>
+							<span className='small card__text__name'>
+								{cardsData.florensis[1].companyName}
+							</span>
+							<span className='card__text__title'>
+								{cardsData.florensis[1].title}
+							</span>
+						</h1>
 						<a href='#blog' className='show-link'>
 							<img src={circle} alt='' className='circle' />
 							<span className='read-more'>Read more</span>
 						</a>
 					</div>
-				</div>
+				</article>
 			</div>
-			<div className='card__container'>
+			<div className='card__grid'>
 				{cardsData.cardImages3.map((card) => (
-					<div className='card__container__image' key={card.id}>
+					<article className='card__article' key={card.id}>
 						<img className='card__image' src={card.image} alt={card.altText} />
-						<div className='card__image__text__container'>
-							<p className='card__image__text card__image__text__name'>
-								{card.companyName}
-							</p>
-							<p className='card__image__text card__image__text__title'>
-								{card.title}
-							</p>
+						<div className='card__text__container'>
+							<h1>
+								<span className='small card__text__name'>{card.companyName}</span>
+								<span className='card__text__title'>{card.title}</span>
+							</h1>
 							<a href='#blog' className='show-link'>
 								<img src={circle} alt='' className='circle' />
 								<span className='read-more'>Read more</span>
 							</a>
 						</div>
-					</div>
+					</article>
 				))}
 			</div>
-			<div className='card__container__quote'>
-				<p className='card__container__quote__description'>
-					“Dept helped us tell our story through a bold new identity and a robust
-					online experience. To the tone of 60% growth in online bookings in just one
-					month”
-				</p>
-				<p className='card__container__quote__name'>
-					MATTIJS TEN DRINK - CEO, TRANSAVIA
-				</p>
+			<div className='quote p-2'>
+				<blockquote>
+					<span className='quote__description'>
+						“Dept helped us tell our story through a bold new identity and a robust
+						online experience. To the tone of 60% growth in online bookings in just
+						one month”
+					</span>
+					<span className='quote__name'>MATTIJS TEN DRINK - CEO, TRANSAVIA</span>
+				</blockquote>
 			</div>
-			<div className='card__container'>
+			<div className='card__grid'>
 				{cardsData.cardImages4.map((card) => (
-					<div className='card__container__image' key={card.id}>
+					<article className='card__article' key={card.id}>
 						<img className='card__image' src={card.image} alt={card.altText} />
-						<div className='card__image__text__container'>
-							<p className='card__image__text card__image__text__name'>
-								{card.companyName}
-							</p>
-							<p className='card__image__text card__image__text__title'>
-								{card.title}
-							</p>
+						<div className='card__text__container'>
+							<h1>
+								<span className='small card__text__name'>{card.companyName}</span>
+								<span className='card__text__title'>{card.title}</span>
+							</h1>
+							<a href='#blog' className='show-link'>
+								<img src={circle} alt='' className='circle' />
+								<span className='read-more'>Read more</span>
+							</a>
 						</div>
-					</div>
+					</article>
 				))}
 			</div>
-		</div>
+		</section>
 	)
 }
